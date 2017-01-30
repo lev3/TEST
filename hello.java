cfe6caf114bb3f662310b19652ba52e8eb17ff9f
viet le <lev3@students.rowan.edu>
2017-01-30 12:35:38 -0500

    test comment

--- hello.java
+++ hello.java
@@ -11,7 +11,7 @@ public class FacePrint {
          System.out.println("" + userChar + " " + userChar);
          System.out.println(" " + userChar);
          System.out.println("" + userChar + "" + userChar + "" + userChar);
-
+	//test comment
          // Get user character
          System.out.print("\nEnter a character ('q' to quit): ");
          usrInput = scnr.next();
