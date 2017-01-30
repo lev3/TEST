3127809b1c1a729346090a1f52daef7529d98aa5
viet le <lev3@students.rowan.edu>
2017-01-30 12:23:43 -0500

    new hello
    new des

---
+++ hello.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FacePrint {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      char userChar = '-'; // User-entered character
+      String usrInput = "";
+
+      while (userChar != 'q') {
+         // Print face
+         System.out.println("" + userChar + " " + userChar);
+         System.out.println(" " + userChar);
+         System.out.println("" + userChar + "" + userChar + "" + userChar);
+
+         // Get user character
+         System.out.print("\nEnter a character ('q' to quit): ");
+         usrInput = scnr.next();
+         userChar = usrInput.charAt(0); // Get the first char in the String
+         System.out.println("");
+      }
+
+      System.out.println("Goodbye.");
+
+      return;
+   }
+}
